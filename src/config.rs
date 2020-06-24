cb7543d83fbb6378c29b13c2448a55ca3dad5f74
inkyu lee <gofiri@gmail.com>
2020-06-24 15:37:56 +0900

    U

---
+++ src/config.rs
@@ -0,0 +1,42 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Config {
+    device_id: String,
+    app_startup_country: String,
+    capabilities: String,
+    device_locale: String,
+    ds_user_id: i32,
+    user_agent: String,
+    mid: String,
+}
+
+impl Config {
+    pub fn fillup_headers(&self, mut request: awc::ClientRequest) -> awc::ClientRequest {
+        request
+            .header("X-IG-Device-ID", self.device_id.clone())
+            .header("X-IG-App-Startup-Country", self.app_startup_country.clone())
+            .header("X-IG-Capabilities", self.capabilities.clone())
+            .header("X-IG-Device-Locale", self.device_locale.clone())
+            .header("X-IG-ABR-Connection-Speed-KBPS", "3")
+            .header("IG-U-Ds-User-ID", self.ds_user_id.to_string())
+            .header("Accept-Language", "en-US;q=1.0")
+            .header("User-Agent", self.user_agent.clone())
+            .header("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8")
+            .header("X-IG-App-Locale", "en")
+            .header("X-IG-Extended-CDN-Thumbnail-Sizes", "249,373,412")
+            .header("X-IG-Bandwidth-Speed-KBPS", "4.110")
+            .header("X-IG-Mapped-Locale", "en_US")
+            .header("X-MID", self.mid.clone())
+            .header("IG-U-Shbid", "14798")
+            .header("IG-U-Shbts", "1592053459.6494713")
+            .header("X-Bloks-Version-Id", "555f61dd0ded5ddf4201c89e12bb453bf605f9bc9d321a69f6e0f11ed9308664")
+            .header("X-IG-Connection-Speed", "137kbps")
+            .header("X-IG-App-ID", "124024574287414")
+            .header("X-IG-Connection-Type", "WiFi")
+            .header("X-Tigon-Is-Retry", "False")
+            .header("Accept-Encoding", "gzip, deflate")
+            .header("X-FB-HTTP-Engine", "Liger")
+            .header("Connection", "close")
+    }    
+}
--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,80 @@
 #[macro_use]
 extern crate log;
 
+mod config;
+use config::Config;
 use std::env;
+use serde_json::json;
 
 #[derive(Debug)]
 enum Error {
     SendRequest(awc::error::SendRequestError),
     Payload(awc::error::PayloadError),
+    YamlParse(serde_yaml::Error),
+    IoError(std::io::Error),
+}
+
+fn encrypt(password: &str) -> String {
+    let mut rng = rand::thread_rng();
+    /*
+    let key = rng.gen::<u8, 32>();
+    let iv = rng.gen::<u8, 12>();
+    */
+    String::from("D")
+    /*
+    let rsa_encrypted = rsa::
+    const rsaEncrypted = crypto.publicEncrypt({
+      key: Buffer.from(this.client.state.passwordEncryptionPubKey, 'base64').toString(),
+      // @ts-ignore
+      padding: crypto.constants.RSA_PKCS1_PADDING,
+    }, randKey);
+    const cipher = crypto.createCipheriv('aes-256-gcm', randKey, iv);
+    const time = Math.floor(Date.now() / 1000).toString();
+    cipher.setAAD(Buffer.from(time));
+    const aesEncrypted = Buffer.concat([cipher.update(password, 'utf8'), cipher.final()]);
+    const sizeBuffer = Buffer.alloc(2, 0);
+    sizeBuffer.writeInt16LE(rsaEncrypted.byteLength, 0);
+    const authTag = cipher.getAuthTag();
+    return {
+      time,
+      encrypted: Buffer.concat([
+        Buffer.from([1, this.client.state.passwordEncryptionKeyId]),
+        iv,
+        sizeBuffer,
+        rsaEncrypted, authTag, aesEncrypted])
+        .toString('base64'),
+    };*/
+
 }
 
 async fn main_async() -> Result<(), Error> {
     use awc::Client;
     use tokio::task::spawn_local;
 
-    let mut client = Client::default();
+    let reader = std::fs::File::open("config.yaml").map_err(Error::IoError)?;
+    let config: Config = serde_yaml::from_reader(reader).map_err(Error::YamlParse)?;
 
-    let mut response = client
-        .post("https://i.instagram.com/api/v1/accounts/login")
-        .header("User-Agent", "Instagram 144.0.0.17.119 (iPhone8,4; iOS 13_5; en_KR; en-KR; scale=2.00; 640x1136; 217426887) AppleWebKit/420+-web")
+    let client = Client::default();
+
+    let request = client.post("https://i.instagram.com/api/v1/accounts/login");
+
+    let mut response = config.fillup_headers(request)
         .send()
-        .await.map_err(Error::SendRequest)?;
+        .await
+        .map_err(Error::SendRequest)?;
 
+    // let a: i32 = response.headers().get("")
     println!("Response: {:?}", response);
+
+    /*
+    let username = "gofiri";
+    let password = "1234";
+    let payload = json!({
+        username: username,
+        password: password,
+        enc_password: format!("#PWD_INSTAGRAM:4:{}:{}", time, encrypted),
+    });
+    */
 
     let body = response.body().await.map_err(Error::Payload)?;
     println!("Downloaded: {:?} bytes", body.len());
